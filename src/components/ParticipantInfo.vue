73e1114f6e4d3628046c17124f7047e0f4a295a2
fernandogelin <fernando@fernandogelin.com>
2020-03-27 11:22:40 -0400

    feat: 🎸 add participant profile and form to update entry

---
+++ src/components/ParticipantInfo.vue
@@ -0,0 +1,99 @@
+<template>
+  <article class="panel is-success content">
+    <h5 class="panel-heading">
+      Participant Profile
+    </h5>
+    <div class="info-group">
+      <aside class="info-group-aside has-background-light">
+        <div class="info-group-item">
+          <p>Name</p>
+        </div>
+        <div class="info-group-item">
+          <p>Year Born</p>
+        </div>
+        <div class="info-group-item">
+          <p>Sex at Birth</p>
+        </div>
+        <div class="info-group-item">
+          <p>Preferred Contact Method</p>
+        </div>
+        <div v-show="participant.email" class="info-group-item">
+          <p>Email</p>
+        </div>
+        <div v-show="participant.phone" class="info-group-item">
+          <p>Phone</p>
+        </div>
+        <div class="info-group-item">
+          <p>Preferred Time of Contact</p>
+        </div>
+      </aside>
+      <main class="info-group-main">
+        <div class="info-group-item">
+          <p>{{ participant.name }}</p>
+        </div>
+        <div class="info-group-item">
+          <p>{{ participant.yob }}</p>
+        </div>
+        <div class="info-group-item">
+          <p>{{ participant.sex_at_birth }}</p>
+        </div>
+        <div class="info-group-item">
+          <p>{{ participant.contact_preference }}</p>
+        </div>
+        <div v-show="participant.email" class="info-group-item">
+          <p>{{ participant.email }}</p>
+        </div>
+        <div v-show="participant.phone" class="info-group-item">
+          <p>{{ participant.phone }}</p>
+        </div>
+        <div class="info-group-item">
+          <ul>
+            <li
+              class="small preferred-time"
+              v-for="(item, index) in participant.preferred_time_of_contact"
+              v-bind:key="index"
+            >
+              {{ item }}
+            </li>
+          </ul>
+        </div>
+      </main>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    participant: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+.panel-heading
+    margin-bottom: 0
+.tag
+    font-weight: bold
+.info-group
+    display: grid
+    grid-template-areas: "aside main"
+    &-aside
+        text-transform: uppercase
+        font-size: 0.7rem
+        padding: 1rem
+        grid-area: aside
+        p
+            padding-top: 0.3rem
+    &-main
+        padding: 1rem
+        grid-area: main
+
+.info-group-item
+    min-height: 2.2rem
+    margin-bottom: 0.2rem
+</style>
--- src/components/UpdateParticipant.vue
+++ src/components/UpdateParticipant.vue
@@ -1,52 +1,90 @@
 <template>
-  <div class="container">
-    <label for="participant-form" class="label is-large"
-      >Personal Information</label
-    >
-    {{ participant.name }}
-    {{ participant.yob }}
-    {{ participant.sex_at_birth }}
-    {{ participant.email }}
-    {{ participant.phone }}
-    {{ participant.contact_preference }}
+  <div class="two-column-grid">
+    <aside class="two-column-grid-side">
+      <ParticipantInfo :participant="participant" />
+    </aside>
+    <main class="two-column-grid-main content">
+      <h1>Study A</h1>
+      <form id="participant-update-form" v-on:submit.prevent="submitForm">
+        <BaseInput
+          id="attempted"
+          label="Attempted Contact Date"
+          type="date"
+          v-model="participant.attempted_contact_date"
+          @blur="$v.participant.attempted_contact_date.$touch()"
+          :error="$v.participant.attempted_contact_date.$error"
+          :valid="!$v.participant.attempted_contact_date.$invalid"
+        />
+        <BaseInput
+          id="contact_method"
+          label="Contact Method"
+          type="text"
+          placeholder="Enter contact method"
+          v-model="participant.contact_method"
+          @blur="$v.participant.contact_method.$touch()"
+          :error="$v.participant.contact_method.$error"
+          :valid="!$v.participant.contact_method.$invalid"
+        />
+        <BaseSelect
+          label="Participant Response"
+          id="sex-at-birth"
+          :options="['-', 'yes', 'no']"
+          v-model="participant.participant_response"
+        />
 
-    <form id="participant-update-form" v-on:submit.prevent="submitForm">
-      <BaseInput
-        id="date"
-        label="date"
-        type="date"
-        placeholder="Attempted Contact Date"
-        v-model="participant.attempted_contact_date"
-      >
-      </BaseInput>
-      <div class="control">
-        <div v-if="success">
-          Form successfully submitted.
-          <font-awesome-icon class="success" icon="check" />
+        <div class="field-inline">
+          <BaseInput
+            class="field"
+            id="start_date"
+            label="Study Participation Start Date"
+            type="date"
+            v-model="participant.participated_start_date"
+            @blur="$v.participant.participated_start_date.$touch()"
+            :error="$v.participant.participated_start_date.$error"
+            :valid="!$v.participant.participated_start_date.$invalid"
+          />
+
+          <BaseInput
+            class="field"
+            id="end_date"
+            label="Study Participation End Date"
+            type="date"
+            v-model="participant.participated_end_date"
+            @blur="$v.participant.participated_end_date.$touch()"
+            :error="$v.participant.participated_end_date.$error"
+            :valid="!$v.participant.participated_end_date.$invalid"
+          />
         </div>
-        <div v-else-if="failed" class="flex">
+
+        <div class="control">
+          <div v-if="success">
+            Data successfully saved.
+            <font-awesome-icon class="success" icon="check" />
+          </div>
+          <div v-else-if="failed" class="flex">
+            <button
+              :disabled="$v.$anyError || $v.participant.$invalid"
+              class="button is-primary"
+              :class="{ loading: loading }"
+            >
+              Save
+            </button>
+            <div>
+              Failed to save form, try again.
+              <font-awesome-icon class="error" icon="exclamation-circle" />
+            </div>
+          </div>
           <button
+            v-else
             :disabled="$v.$anyError || $v.participant.$invalid"
             class="button is-primary"
             :class="{ loading: loading }"
           >
-            Submit
+            Save
           </button>
-          <div>
-            Failed to submit form, try again.
-            <font-awesome-icon class="error" icon="exclamation-circle" />
-          </div>
         </div>
-        <button
-          v-else
-          :disabled="$v.$anyError || $v.participant.$invalid"
-          class="button is-primary"
-          :class="{ loading: loading }"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
+      </form>
+    </main>
   </div>
 </template>
 
@@ -54,10 +92,14 @@
 import store from '@/store/index'
 import { validationMixin } from 'vuelidate'
 import { required } from 'vuelidate/lib/validators'
-
 import { mapState } from 'vuex'
 
+import ParticipantInfo from '@/components/ParticipantInfo'
+
 export default {
+  components: {
+    ParticipantInfo
+  },
   mixins: [validationMixin],
   data() {
     return {
@@ -74,14 +116,22 @@ export default {
     participant: {
       attempted_contact_date: {
         required
+      },
+      contact_method: {
+        required
+      },
+      participated_start_date: {
+        required
+      },
+      participated_end_date: {
+        required
       }
     }
   },
   methods: {
     submitForm: async function() {
       await this.$recaptchaLoaded()
-      const token = await this.$recaptcha('submit')
-      console.log(token)
+      this.$recaptcha('submit')
       this.loading = true
       store.dispatch('firebase/updateParticipant', this.participant).then(
         () => {
@@ -97,3 +147,25 @@ export default {
   }
 }
 </script>
+
+<style lang="sass" scoped>
+.two-column-grid
+    display: grid
+    grid-template-areas: "side . main"
+    grid-template-columns: 6fr 0.2fr 8fr
+    grid-template-rows: auto
+    &-side
+        grid-area: side
+        padding: 1rem
+    &-main
+        grid-area: main
+        padding: 1rem
+
+.field-inline
+  display: flex
+  flex-direction: row
+  flex-wrap: wrap
+  justify-content: flex-start
+  .field
+    margin-right: 2rem
+</style>
