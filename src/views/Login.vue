fce16fd98b1c666836f71f48d66a873d8d672c19
Isabel Restrepo <isabelrestre@gmail.com>
2021-02-25 10:05:12 -0500

    fix(login): no more uncaught promises thanks to @mcmcgrath13

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,6 +5,8 @@
     <main class="login content">
       <div class="login-container">
         <h1>Researcher Login</h1>
+        <p>Please login with you Brown Gmail Account</p>
+        <p>Only resgistered researchers can login</p>
         <BaseNotification
           v-if="showNotification"
           :text="message"
@@ -13,33 +15,10 @@
         >
         </BaseNotification>
         <form @submit.prevent="login">
-          <BaseInput
-            id="email"
-            label="Email"
-            type="email"
-            placeholder="Email"
-            v-model="form.email"
-            @blur="$v.form.email.$touch()"
-            :error="$v.form.email.$error"
-            :valid="!$v.form.email.$invalid"
-          >
-          </BaseInput>
-          <BaseInput
-            id="password"
-            label="Password"
-            type="password"
-            placeholder="Password"
-            v-model="form.password"
-            @blur="$v.form.password.$touch()"
-            :error="$v.form.password.$error"
-            :valid="!$v.form.password.$invalid"
-          >
-          </BaseInput>
           <button class="button is-success" type="submit">
             Login
           </button>
         </form>
-        <router-link to="/reset">Reset your password</router-link>
       </div>
     </main>
   </div>
@@ -85,59 +64,56 @@ export default {
     }
   },
   methods: {
-    validAccountCheck(response) {
+    async validAccountCheck(response) {
       let user = firebase.auth().currentUser
       console.log(user)
       console.log(user.email)
       console.log(response)
       //   const respUser = response.user.email
-      return db
-        .collection('admin')
-        .doc(user.email)
-        .get()
-        .then(() => {
-          this.$store.dispatch('login/authenticate', user.email)
-          this.$store.dispatch('firebase/bindStudies')
-          this.$store.dispatch('firebase/bindParticipants')
-          return true
-        })
-        .catch(err => {
-          // Not a white-listed user
-          console.log(err)
-          return false
-        })
+      try {
+        await db
+          .collection('admin')
+          .doc(user.email)
+          .get()
+        this.$store.dispatch('login/authenticate', user.email)
+        this.$store.dispatch('firebase/bindStudies')
+        this.$store.dispatch('firebase/bindParticipants')
+        return true
+      } catch (err) {
+        console.log('Not authorized1')
+        console.log(err)
+        return false
+      }
     },
 
-    login() {
-      let that = this
-      var provider = new firebase.auth.GoogleAuthProvider()
-      firebase
-        .auth()
-        .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-        .then(async () => {
-          await firebase
-            .auth()
-            // .signInWithEmailAndPassword(this.form.email, this.form.password)
-            .signInWithPopup(provider)
-            .then(response => {
-              this.validAccountCheck(response).then(validAccount => {
-                if (validAccount) {
-                  console.log('Valid User')
-                } else {
-                  firebase.auth().signOut()
-                  console.log('Invalid User')
-                  throw new Error('Error text.')
-                }
-              })
-            })
-          this.$router.replace({ name: 'dashboard' })
-        })
-        .catch(err => {
-          that.error = err.message
-          that.showNotification = true
-          that.message = that.errorMessage
-          that.notificationVariant = 'danger'
-        })
+    async login() {
+      try {
+        var provider = new firebase.auth.GoogleAuthProvider()
+        await firebase
+          .auth()
+          .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+
+        let response = await firebase
+          .auth()
+          // .signInWithEmailAndPassword(this.form.email, this.form.password)
+          .signInWithPopup(provider)
+
+        let validAccount = await this.validAccountCheck(response)
+
+        if (!validAccount) {
+          await firebase.auth().signOut()
+          throw new Error('Invalid User')
+        }
+        this.$router.replace({ name: 'dashboard' })
+      } catch (err) {
+        console.log('Not authorized2')
+
+        console.log(err)
+        this.error = err.message
+        this.showNotification = true
+        this.message = this.errorMessage
+        this.notificationVariant = 'danger'
+      }
     },
     hideNotification() {
       this.showNotification = true
